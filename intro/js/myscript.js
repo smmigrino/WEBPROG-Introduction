5ff09620e77d6d8349a200bfe3357cc747d6dfdc
smmigrino <smmigrino@student.apc.edu.ph>
2025-04-14 10:53:43 +0800

    js change color and font

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
+    document.getElementById("p2").style.color="blue";
+    document.getElementById("p2").style.fontFamily="Ariel";
+    document.getElementById("p2").style.fontSize="larger";
+
+
 }
