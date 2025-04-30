bcde5fddf915da0529026279516c35aa5c6aeec5
smmigrino <smmigrino@student.apc.edu.ph>
2025-04-30 20:03:19 +0800

    update, go back

--- intro/components/gallery_vue.js
+++ intro/components/gallery_vue.js
@@ -14,11 +14,24 @@ createApp({
   },
   template: `
     <div class="gallery-wrapper">
-      <div class="grid-gallery">
-        <div v-for="(photo, index) in photos" :key="index" :class="'item' + (index + 1)">
+      <div class="collage">
+        <div v-for="(photo, index) in photos" :key="index" class="collage-item" :style="getItemStyle(index)">
           <img :src="photo.src" :alt="'Photo ' + (index + 1)" />
         </div>
       </div>
     </div>
-  `
+  `,
+  methods: {
+    getItemStyle(index) {
+      const styles = [
+    { flex: '1 1 45%', height: '150px' },
+    { flex: '1 1 30%', height: '150px' },
+    { flex: '1 1 40%', height: '150px' },
+    { flex: '1 1 30%', height: '150px' },
+    { flex: '1 1 30%', height: '185px' }
+      ];
+      
+      return styles[index] || { flex: '1 1 45%', height: '150px' };
+    }
+  }
 }).mount('#gallery-app');
