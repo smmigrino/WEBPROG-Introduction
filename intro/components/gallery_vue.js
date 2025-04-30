aaf9fb8e2a528c3dbe877d4fafa9148d953b05ea
smmigrino <smmigrino@student.apc.edu.ph>
2025-04-30 15:00:37 +0800

    update size

--- intro/components/gallery_vue.js
+++ intro/components/gallery_vue.js
@@ -24,10 +24,10 @@ createApp({
   methods: {
     getItemStyle(index) {
       const styles = [
-        { flex: '1 1 45%', height: '200px' },
-        { flex: '1 1 50%', height: '230px' },
-        { flex: '1 1 40%', height: '250px' },
-        { flex: '1 1 70%', height: '300px' },
+        { flex: '1 1 45%', height: '80px' },
+        { flex: '1 1 30%', height: '80px' },
+        { flex: '1 1 40%', height: '150px' },
+        { flex: '1 1 30%', height: '200px' },
         { flex: '1 1 45%', height: '280px' }
       ];
       return styles[index] || { flex: '1 1 45%', height: '150px' };
